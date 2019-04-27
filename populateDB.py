00ca02a0fd2b4053fc2d9f0caeef8f4088487fb6
Saeed <saeed.abdellwahab@gmail.com>
2019-04-27 12:15:01 +0200

    fix: Increase the items populated
    When you open the server with not enough items, an error occures which states out of range, so by increasing the population it was fixed

--- populateDB.py
+++ populateDB.py
@@ -16,8 +16,34 @@ dbsession.commit()
 cat = Category(name="Soccer")
 dbsession.add(cat)
 dbsession.commit()
+newItem = Item(name="Ball",
+               category_id=cat.id,
+               description="A round and black object")
+dbsession.add(newItem)
+dbsession.commit()
+newItem = Item(name="Pitch",
+               category_id=cat.id,
+               description="A vast area for playing")
+dbsession.add(newItem)
+dbsession.commit()
+newItem = Item(name="Shoes",
+               category_id=cat.id,
+               description="An item wore in feet to facilitate running")
+dbsession.add(newItem)
+dbsession.commit()
+
 cat = Category(name="Swimming")
 dbsession.add(cat)
+dbsession.commit()
+newItem = Item(name="Swimsuit",
+               category_id=cat.id,
+               description="An item wore for swimming")
+dbsession.add(newItem)
+dbsession.commit()
+newItem = Item(name="Board",
+               category_id=cat.id,
+               description="An item used for swimming")
+dbsession.add(newItem)
 dbsession.commit()
 cat = Category(name="Hunting")
 dbsession.add(cat)
